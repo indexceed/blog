ed22ac25436047834f3adaa3aab9c013f500396f
Christian Franco <c.franco@indexceed.com>
2022-10-06 16:21:19 -0500

    feat: title & menu deisng

---
+++ src/BlogApp.tsx
@@ -0,0 +1,37 @@
+
+
+export const BlogApp = () => {
+
+
+  return (
+    <>
+      <div>
+        <div className="head"> 
+          <div className="dropdown">
+              <span>Menu</span>
+              <div className="dropdown-content">
+                  <nav>
+                    <ul>
+                      <li> <a href="defautl">Default</a></li>
+                      <li> <a href="news">News</a></li>
+                      <li> <a href="contact">Contact</a></li>
+                      <li> <a href="about">About</a></li>
+                    </ul>
+                  </nav>
+              </div>
+          </div>
+          <div className="title">
+            <h1>BlogX</h1>
+          </div>
+          
+        </div>
+
+         
+      </div>
+       
+    </>
+  )
+}
+
+
+
